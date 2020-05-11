01aa2d3c5e0669a46035702002852ed323afb9dd
yijie4188 <yijie4188@gmail.com>
2020-05-12 07:08:08 +0800

    pyqt5库对系统变量的加载存在bug

--- start.py
+++ start.py
@@ -8,6 +8,9 @@
 @Contact        :   yijie4188@gmail.com
 @Desciption     :   可视化编辑启动文件
 '''
+import sys, os
+if hasattr(sys, 'frozen'):
+    os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
 import visualizationSrc.Main as Main
 
 Main.mainWindowStart()
