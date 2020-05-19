4aa3f1fe89c5f12903384281a084d29a4b00a6fa
yijie4188 <yijie4188@gmail.com>
2020-05-20 03:04:51 +0800

    添加upx压缩(然而没什么屁用) 删除老版本处理excel部分的代码

--- visualizationSrc/Controler/CardControler.py
+++ visualizationSrc/Controler/CardControler.py
@@ -8,63 +8,6 @@
 @Contact        :   yijie4188@gmail.com
 @Desciption     :   
 '''
-# import win32com
-# from win32com.client import Dispatch
-#
-# # 进程可见，False是它后台
-# Visible = False
-#
-# def excel_open(filePath, sheetName, fun, VBA=""):
-#     xlApp = win32com.client.DispatchEx("Excel.Application")
-#     xlApp.Visible = Visible
-#     xlApp.DisplayAlerts = 0
-#     try:
-#         xlBook = xlApp.Workbooks.Open(filePath,False)
-#         sht = xlBook.Worksheets(sheetName)
-#         fun(sht)
-#         print("WRITE FINISHED")
-#         xlBook.Close(True)
-#         if VBA !="":
-#             useVBA(xlApp, filePath, VBA)
-#             print("VBA FINISHED")
-#     except Exception as e:
-#         print(e)
-#     finally:
-#         xlApp.Quit()
-#         return 0
-# def useVBA(xlApp, filePath, VBA):
-#     xlBook = xlApp.Workbooks.Open(filePath,False)
-#     xlBook.Application.Run(VBA)
-#     xlBook.Close(True)
-#
-# from ..Util.frozenDir import appPath
-# temp = appPath()
-# proHome = temp[0] + (lambda : "" if temp[1] else "../../../")()
-# DatabaseXlsPath = proHome+'Database/Database.xls'
-#
-# def insertCard(c):
-#     filePath = DatabaseXlsPath
-#     # c = Card(
-#     #     id=10001, displayName="转运咒语", price=100, energyReq=0,
-#     #     range=0,
-#     #     description="传送至灰雾之上1个回合",
-#     #     code="LoadLevel:get_currentLevelId:;\nLog:get_currentLevelId:;",
-#     #     story0="“福生玄黄仙尊。”\n“福生玄黄天君。”\n“福生玄黄上帝。”\n“福生玄黄天尊。”")
-#     def __insertCard(sht):
-#         preCell = None
-#         for i in range(1,sht.usedrange.rows.count):
-#             cell = sht.Cells(i+1, 1)
-#             if cell.Value == None:
-#                 if i == 1: preId = 10000
-#                 else: preId = preCell.Value
-#                 c.id = preId + 1
-#                 t = c.toTuple()
-#                 for j in range(t.__len__()):
-#                     if t[j] != "":sht.Cells(i + 1, j+1).Value = t[j]
-#                 break
-#             preCell = cell
-#     return excel_open(filePath, "Card", __insertCard, "CsvExportBook")
-
 import csv
 
 class CardControler:
