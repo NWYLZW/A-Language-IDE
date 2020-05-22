79aba71f637cbaa595ae0fcc75b5d05df83c4794
yijie4188 <yijie4188@gmail.com>
2020-05-23 04:01:26 +0800

    完成选择用户游戏本体文件夹

--- visualizationSrc/Tabs/HomeTab.py
+++ visualizationSrc/Tabs/HomeTab.py
@@ -8,10 +8,13 @@
 @Contact        :   yijie4188@gmail.com
 @Desciption     :   主页
 '''
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from PyQt5.uic.properties import QtWidgets
 
 from ..Controler.ContentTabListControler import ContentTabList
+from ..Util.UserUtil import UserUtil
+from ..Util.frozenDir import appPath
+from ..config import GamePath
 from ..qtUI.mainInterFace import Ui_MainWindow
 
 class Home:
@@ -20,6 +23,24 @@ class Home:
         self.mainWindow = mainWindow
         self.CTL = CTL
         self.initCardItemClick()
+        self.setSimpleData()
+        self.initClick()
+    def setSimpleData(self):
+        u = UserUtil()
+        temp = appPath().split('\\')
+        self.UI.ModName.setText(temp[len(temp)-1])
+        tempGamePath = GamePath
+        if len(GamePath)>30:
+            tempGamePath = GamePath[:14]+'...'+GamePath[-14:]
+        self.UI.GamePath.setText(tempGamePath)
+        self.UI.GamePath.setToolTip(GamePath)
+        self.UI.UserName.setText(u.userName)
+    def initClick(self):
+        def sel_GamePath():
+            directory = QFileDialog.getExistingDirectory(None, "getExistingDirectory", "C:\\")
+            UserUtil().gamePath = directory
+            self.setSimpleData()
+        self.UI.sel_GamePath.clicked.connect(sel_GamePath)
     def initCardItemClick(self):
         UI = self.UI
         from PyQt5 import QtCore
